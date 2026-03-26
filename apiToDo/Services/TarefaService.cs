9a004a421447080e7c10ba3a2e4fae843ea32e6e
guilhermecampoz <augusto.locam@gmail.com>
2026-03-26 20:56:29 -0300

    Remove try catch não utilizado do service para não confundir o stack trace

--- apiToDo/Services/TarefaService.cs
+++ apiToDo/Services/TarefaService.cs
@@ -18,98 +18,66 @@ namespace apiToDo.Services
 
         public Result<List<Tarefa>> ListarTarefas()
         {
-            try
-            {
-                return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+
+            return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
         }
 
         public Result<Tarefa> ObterTarefa(int id)
         {
-            try
-            {
-                var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
-                if (tarefa == null)
-                    return Result<Tarefa>.Falha(Erro.NotFound, new List<string> { "Não foi encontrado tarefa com o Id informado." });
+            var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
+            if (tarefa == null)
+                return Result<Tarefa>.Falha(Erro.NotFound, new List<string> { "Não foi encontrado tarefa com o Id informado." });
 
-                return Result<Tarefa>.Sucesso(tarefa);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return Result<Tarefa>.Sucesso(tarefa);
         }
 
         public Result<List<Tarefa>> InserirTarefa(TarefaDTO request)
         {
-            try
+            if (request == null || string.IsNullOrEmpty(request.Titulo))
             {
-                if (request == null || string.IsNullOrEmpty(request.Titulo))
-                {
-                    return Result<List<Tarefa>>.Falha(Erro.Validation, new List<string> { "O nome da tarefa não pode ser vazio." });
-                }
-
-                int id = _data.Count;
-                Tarefa tarefa = new Tarefa();
-                tarefa.Titulo = request.Titulo;
-                tarefa.Id = id;
-                _data.Tarefas.Add(tarefa);
-
-                return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
+                return Result<List<Tarefa>>.Falha(Erro.Validation, new List<string> { "O nome da tarefa não pode ser vazio." });
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+
+            int id = _data.Count;
+            Tarefa tarefa = new Tarefa();
+            tarefa.Titulo = request.Titulo;
+            tarefa.Id = id;
+            _data.Tarefas.Add(tarefa);
+
+            return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
         }
+
         // recebe via parâmetro o id da tarefa informada na query
         public Result<List<Tarefa>> DeletarTarefa(int id)
         {
-            try
-            {
-                // pesquisa no "banco" se há alguma tarefa com o Id informado na query
-                var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
-                // Valida se não encontrou
-                if (tarefa == null)
-                    // quando não encontra retorna falha do result pattern com tipo de erro NotFound para poder usar o status code correto na controller
-                    return Result<List<Tarefa>>.Falha(Erro.NotFound, new List<string> { "Não foi encontrada uma tarefa com o Id informado." });
-                // se encontrar remove a tarefa via referência
-                _data.Tarefas.Remove(tarefa);
-                // retorna sucesso com a lista de tarefas atualizadas em memória da aplicação que foi configurada usando singleton na injeção de dependências
-                return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            // pesquisa no "banco" se há alguma tarefa com o Id informado na query
+            var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
+            // Valida se não encontrou
+            if (tarefa == null)
+                // quando não encontra retorna falha do result pattern com tipo de erro NotFound para poder usar o status code correto na controller
+                return Result<List<Tarefa>>.Falha(Erro.NotFound, new List<string> { "Não foi encontrada uma tarefa com o Id informado." });
+            // se encontrar remove a tarefa via referência
+            _data.Tarefas.Remove(tarefa);
+            // retorna sucesso com a lista de tarefas atualizadas em memória da aplicação que foi configurada usando singleton na injeção de dependências
+            return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
         }
 
         public Result<List<Tarefa>> AtualizarTarefa(int id, TarefaDTO request)
         {
-            try
+
+            if (request == null || string.IsNullOrEmpty(request.Titulo))
             {
-                if (request == null || string.IsNullOrEmpty(request.Titulo))
-                {
-                    return Result<List<Tarefa>>.Falha(Erro.Validation, new List<string> { "O nome da tarefa não pode ser vazio." });
-                }
-
-                var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
-
-                if (tarefa == null)
-                    return Result<List<Tarefa>>.Falha(Erro.NotFound, new List<string> { "Não foi encontrada uma tarefa com o Id informado." });
-
-                tarefa.Titulo = request.Titulo;
-
-                return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
+                return Result<List<Tarefa>>.Falha(Erro.Validation, new List<string> { "O nome da tarefa não pode ser vazio." });
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+
+            var tarefa = _data.Tarefas.FirstOrDefault(x => x.Id == id);
+
+            if (tarefa == null)
+                return Result<List<Tarefa>>.Falha(Erro.NotFound, new List<string> { "Não foi encontrada uma tarefa com o Id informado." });
+
+            tarefa.Titulo = request.Titulo;
+
+            return Result<List<Tarefa>>.Sucesso(_data.Tarefas);
         }
     }
 }
